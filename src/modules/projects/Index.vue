a550e5a4a74a722f539423c96ab7ffd3e622a142
RAlekseev <roma99@bk.ru>
2022-09-17 14:33:53 +0300

    vue-router + project structure

---
+++ src/modules/projects/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    Projects
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex',
+  components: {
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
