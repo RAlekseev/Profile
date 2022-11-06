45e35693fc96f837ecf87b8f01fd8c9c9df71cc3
RAlekseev <roma99@bk.ru>
2022-11-06 19:30:07 +0600

    vue-asset-generate icons

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    manifestPath: 'manifest.json'
+    manifestPath: 'img/icons/manifest.json'
   }
 })
