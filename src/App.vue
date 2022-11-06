12d5bb7c16e401d73687bd1ebe16167a977862ea
RAlekseev <roma99@bk.ru>
2022-11-06 21:35:31 +0600

    Header, Footer Styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <header>
+      <nav>
+        <router-link to="/">Главная</router-link> |
+        <router-link to="/projects">Мои проекты</router-link> |
+        <router-link to="/my_way">Мой путь</router-link>
+      </nav>
+    </header>
+    <router-view/>
+    <footer>
+      <p>© 2022 with ♥</p>
+    </footer>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="hello">
-    <h4>
-      <li v-for="project in projects" :key="project.id">
-        {{ project.name }}
-        <template v-for="techVer in project.technologies" :key="techVer.id">
-          {{ technology(techVer.id).name }}: {{ techVer.version }}
-        <br>
-        </template>
-      </li>
-    </h4>
+  <div>
+
   </div>
 </template>
 
